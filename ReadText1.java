4834f24dbeba1e1c979b97f1d8408a22ffbf9a5d
junseo04 <qkrwnstj0601@gmail.com>
2020-03-23 12:13:56 +0900

    DxfReader

--- ReadText1.java
+++ ReadText1.java
@@ -2,29 +2,30 @@ package location;
  
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.DrbgParameters.NextBytes;
 import java.util.Scanner;
  
-public class ReadText1 {
+public class ReadText3 {
+  
     public static void main(String[] args){
         try{
-        	int a=8;
+            String [] x = {RDL_STLT_PS, POINT };
+            String text = "RDL_STLT_PS ";
+            
             //파일 객체 생성
             File file = new File("C:\\Users\\USER\\Desktop\\point\\point1.txt");
             //스캐너로 파일 읽기
             Scanner scan = new Scanner(file);
-            while(scan.hasNextLine()) {
-                System.out.println(scan.nextLine());
-                if(scan.nextInt()==a) {
-                	
-                	 while(scan.hasNextLine()) {
-                         System.out.println(scan.nextLine());
-                	 }
-                	
-                }
-            }
-            //System.out.println(scan.useDelimiter("\\z").next());
+          
+                    System.out.println(scan.nextLine());
+              
+                
+           
+          //System.out.println(scan.useDelimiter("\\z").next());
         }catch (FileNotFoundException e) {
             // TODO: handle exception
         }
     }
 }
+
+
